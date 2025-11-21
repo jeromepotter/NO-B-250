9c5675f4ff556f33df3984fbb12005db4c8c4b8d
Jerome Potter <jerome.potter@gmail.com>
2025-11-20 23:42:24 -0800

    Tremolo Ring Mod

--- js/synth-processor.js
+++ js/synth-processor.js
@@ -317,8 +317,8 @@ for(let i=0;i<oL.length;i++){
     
     const dV=currentParams[1]; if (dV>0.01){ const dr=1+dV*19; const k=2*dr/(1+dr); s_L=(1+k)*s_L/(1+k*Math.abs(s_L)); s_R=(1+k)*s_R/(1+k*Math.abs(s_R)); const nS=Math.max(2,Math.floor(Math.pow(1-dV,2.5)*64)); const sS=2.0/nS; s_L=sS*Math.floor(s_L/sS+0.5); s_R=sS*Math.floor(s_R/sS+0.5); const gC=1/(1+dV*1.5); s_L*=gC; s_R*=gC; }
     if(currentParams[5] > 0){ 
-        const tremRateHz = 2 + (currentParams[5] * 8); 
-        const tD = currentParams[5] * 0.8; 
+         const tremRateHz = 2 + (Math.pow(currentParams[5], 3) * 500);
+         const tD = currentParams[5] * 0.8; 
         const t = Math.sin(this.tremoloPhase) * tD + (1.0 - tD); 
         s_L *= t; 
         s_R *= t; 
@@ -368,5 +368,6 @@ return true;
 }
 }
 registerProcessor('synth-processor', SynthProcessor);
+
 
 
