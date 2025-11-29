56888a3f3c52ae7765e1b0b5de93fca2e9d09703
Jerome Potter <jerome.potter@gmail.com>
2025-11-29 11:43:23 -0800

    Restore previous DJ filter curve

--- js/synth-processor.js
+++ js/synth-processor.js
@@ -465,10 +465,9 @@ case 'ping':
                    }
 
                    const Q = 0.707 + Math.pow(res, 2) * 24;
-                   const shapedAmount = Math.pow(amount, 2.5);
                    const freqNorm = centerOffset < 0
-                       ? 1 - shapedAmount
-                       : shapedAmount;
+                       ? Math.pow(1 - amount, 3)
+                       : Math.pow(amount, 3);
                    const w = 2 * Math.PI * (40 + freqNorm * (sampleRate / 2.2 - 40)) / sampleRate;
                    const s = Math.sin(w);
                    const cosw = Math.cos(w);
